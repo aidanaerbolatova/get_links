ebd4987a2b07ae0c99ba63d0286c169053ba89a4
aidanaerbolatova <erbolatovaaidanaa@gmail.com>
2023-05-30 14:40:23 +0600

    connection

--- internal/repository/connection.go
+++ internal/repository/connection.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"test/internal/models"
 
 	"github.com/jmoiron/sqlx"
@@ -9,4 +10,14 @@ import (
 
 func ConnectDB(logger *zap.SugaredLogger, config *models.Config) (*sqlx.DB, error) {
 	return NewPostgresDB(logger, config)
+}
+
+func NewPostgresDB(logger *zap.SugaredLogger, cfg *models.Config) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+	if err != nil {
+		logger.Errorf("Error while connect to Postgres: %v", err)
+		return nil, err
+	}
+
+	return db, nil
 }
